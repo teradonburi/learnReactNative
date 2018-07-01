23e3bf002218a4088fe469b831e9f1a573bf0a04
teradonburi <daikiterai@gmail.com>
2018-07-01 15:01:06 +0900

    fix

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 })
 
-type Props = {};
+type Props = {}
 export default class App extends Component<Props> {
   render() {
     return (
