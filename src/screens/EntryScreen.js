f5a74b61d15974d34785ae8b55605559cb5122d5
teradonburi <daikiterai@gmail.com>
2018-07-08 15:07:54 +0900

    fix

--- src/screens/EntryScreen.js
+++ src/screens/EntryScreen.js
@@ -9,7 +9,6 @@ import { connect } from 'react-redux'
 import { View, StyleSheet } from 'react-native'
 import { Container, Item, Header, Body, Title, Content, Right, Footer, FooterTab, Card, Input, Button, Text } from 'native-base'
 import { Field, reduxForm } from 'redux-form'
-import isEmail from 'validator/lib/isEmail'
 import { update } from '../modules/user'
 
 const red = '#FF0000'
@@ -30,6 +29,24 @@ const renderInput = ({ input, placeholder, meta: { touched, error } }) => {
       {hasError ? <Text style={styles.error}>{error}</Text> : <Text />}
     </View>
   )
+}
+
+// メールアドレス判定
+function isEmail(mail) {
+  const mail_regex1 = new RegExp('(?:[-!#-\'*+/-9=?A-Z^-~]+\.?(?:\.[-!#-\'*+/-9=?A-Z^-~]+)*|"(?:[!#-\[\]-~]|\\\\[\x09 -~])*")@[-!#-\'*+/-9=?A-Z^-~]+(?:\.[-!#-\'*+/-9=?A-Z^-~]+)*') // eslint-disable-line
+  const mail_regex2 = new RegExp('^[^\@]+\@[^\@]+$')
+  if (mail.match(mail_regex1) && mail.match(mail_regex2)) {
+    // 全角チェック
+    if (mail.match(/[^a-zA-Z0-9\!\"\#\$\%\&\'\(\)\=\~\|\-\^\\\@\[\;\:\]\,\.\/\\\<\>\?\_\`\{\+\*\} ]/)) {
+      return false
+    }
+        // 末尾TLDチェック（〜.co,jpなどの末尾ミスチェック用）
+    if (!mail.match(/\.[a-z]+$/)) {
+      return false
+    }
+    return true
+  }
+  return false
 }
 
 @reduxForm({
